d2f6ec242fb60b51f64e9537beb264f48efaebd6
ozgurarik <math.ozgurarik@gmail.com>
2023-08-19 21:38:11 +0300

    first commit

---
+++ src/main/java/ders_gecme.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ders_gecme {
+    public static void main(String[] args) {
+        double mat, fizik, kimya, turkce, biyoloji;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("matematik notunuz: ");
+        mat = input.nextDouble();
+        System.out.print("fizik notunuz: ");
+        fizik = input.nextDouble();
+        System.out.print("kimya notunuz: ");
+        kimya = input.nextDouble();
+        System.out.print("biyoloji notunuz: ");
+        biyoloji = input.nextDouble();
+        System.out.print("turkce notunuz: ");
+        turkce = input.nextDouble();
+
+        double ortalama = (mat + fizik + kimya + biyoloji + turkce) / 5;
+        if (ortalama < 55) {
+            System.out.println("sınıfta kaldınız.. :(");
+        } else {
+            System.out.println("sınıfı geçtiniz.. :)");
+        }
+        System.out.print("ortalamanız: " + ortalama);
+
+    }
+}
