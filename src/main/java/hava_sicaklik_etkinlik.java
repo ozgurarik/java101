6053bb899b60a803a578c71aa97c53aea37468a8
ozgurarik <math.ozgurarik@gmail.com>
2023-08-20 15:27:23 +0300

    yeni kod eklendi

--- src/main/java/hava_sicaklik_etkinlik.java
+++ src/main/java/hava_sicaklik_etkinlik.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 public class hava_sicaklik_etkinlik {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Sıcaklık değerini giriniz: ");
-        int sicaklik = scanner.nextInt();
+        int sicaklik = input.nextInt();
 
         if (sicaklik < 5) {
-            System.out.println("Kayak yapmayı öner.");
+            System.out.println("Kayak yapabilirsiniz.");
         }
         if (sicaklik >= 5 && sicaklik <= 15) {
-            System.out.println("Sinema etkinliğini öner.");
+            System.out.println("Sinemaya gidebilirsiniz.");
         }
         if (sicaklik >= 10 && sicaklik <= 25) {
-            System.out.println("Piknik etkinliğini öner.");
+            System.out.println("Pikniğe gidebilirsiniz.");
         } else {
-            System.out.println("Yüzme etkinliğini öner.");
+            System.out.println("Yüzmeye gidebilirsiniz.");
         }
     }
 }
