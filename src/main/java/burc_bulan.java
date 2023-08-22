52413fafd36eba2878634abf60be514ca4d3ea44
ozgurarik <math.ozgurarik@gmail.com>
2023-08-22 15:27:18 +0300

    second commit

--- src/main/java/burc_bulan.java
+++ src/main/java/burc_bulan.java
@@ -22,17 +22,17 @@ public class burc_bulan {
             burc = "Aslan";
         } else if ((ay == 8 && (gun >= 23 && gun <= 31)) || (ay == 9 && (gun <= 22 && gun >= 1))) {
             burc = "Başak";
-        } else if ((ay == 9 && gun >= 23) || (ay == 10 && gun <= 22)) {
+        } else if ((ay == 9 && (gun >= 23 && gun <= 30)) || (ay == 10 && (gun <= 22 && gun >= 1))) {
             burc = "Terazi";
-        } else if ((ay == 10 && gun >= 23) || (ay == 11 && gun <= 21)) {
+        } else if ((ay == 10 && (gun >= 23 && gun <= 31)) || (ay == 11 && (gun >= 1 && gun <= 21))) {
             burc = "Akrep";
-        } else if ((ay == 11 && gun >= 22) || (ay == 12 && gun <= 21)) {
+        } else if ((ay == 11 && (gun >= 22 && gun <= 30)) || (ay == 12 && (gun >= 1 && gun <= 21))) {
             burc = "Yay";
-        } else if ((ay == 12 && gun >= 22) || (ay == 1 && gun <= 21)) {
+        } else if ((ay == 12 && (gun <= 31 && gun >= 22)) || (ay == 1 && (gun <= 21 && gun >= 1))) {
             burc = "Oğlak";
-        } else if ((ay == 1 && gun >= 22) || (ay == 2 && gun <= 19)) {
+        } else if ((ay == 1 && (gun <= 31 && gun >= 22)) || (ay == 2 && (gun <= 19 && gun >= 1))) {
             burc = "Kova";
-        } else if ((ay == 2 && gun >= 20) || (ay == 3 && gun <= 20)) {
+        } else if ((ay == 2 && (gun <= 29 && gun >= 20)) || (ay == 3 && (gun >= 1 && gun <= 20))) {
             burc = "Balık";
         } else {
             System.out.print("geçerli bir veri giriniz...");
