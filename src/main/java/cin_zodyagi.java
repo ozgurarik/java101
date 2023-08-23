020c10da8cdb97a405ea6fb70f4502629344bc34
ozgurarik <math.ozgurarik@gmail.com>
2023-08-23 22:25:19 +0300

    kod eklendi

--- src/main/java/cin_zodyagi.java
+++ src/main/java/cin_zodyagi.java
@@ -37,3 +37,38 @@ public class cin_zodyagi {
         System.out.print("çin zodyağı burcunuz.: " + zodyakKod);
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class cin_zodyagi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğum yılınızı giriniz: ");
+        int dogumYili = input.nextInt();
+
+        int zodyakKodu = dogumYili % 12;
+
+        String zodyakBurcu = "";
+
+        switch (zodyakKodu) {
+            case 0: zodyakBurcu = "Maymun"; break;
+            case 1: zodyakBurcu = "Horoz"; break;
+            case 2: zodyakBurcu = "Köpek"; break;
+            case 3: zodyakBurcu = "Domuz"; break;
+            case 4: zodyakBurcu = "Fare"; break;
+            case 5: zodyakBurcu = "Öküz"; break;
+            case 6: zodyakBurcu = "Kaplan"; break;
+            case 7: zodyakBurcu = "Tavşan"; break;
+            case 8: zodyakBurcu = "Ejderha"; break;
+            case 9: zodyakBurcu = "Yılan"; break;
+            case 10: zodyakBurcu = "At"; break;
+            case 11: zodyakBurcu = "Koyun"; break;
+            default: zodyakBurcu = "Geçersiz"; break;
+        }
+           System.out.println("Çin Zodyağı Burcunuz: " + zodyakBurcu);
+    }
+}
+
+ */
