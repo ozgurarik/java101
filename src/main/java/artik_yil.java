14bee005aa0b67d3d3badfbc87f025514f346934
ozgurarik <math.ozgurarik@gmail.com>
2023-08-24 11:42:13 +0300

    first commit

--- src/main/java/artik_yil.java
+++ src/main/java/artik_yil.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;public class artik_yil {
-public static void main(String[] args){
-}
+import java.util.Scanner;
+
+public class artik_yil {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("bir yıl giriniz : ");
+        int yil = input.nextInt();
+
+        if ((yil % 4 == 0 && yil % 100 != 0) || (yil % 400 == 0)) {
+            System.out.println(yil + " artık yıldır. ");
+        } else {
+            System.out.println(yil + " artık yıl değildir .");
+        }
+    }
 }
